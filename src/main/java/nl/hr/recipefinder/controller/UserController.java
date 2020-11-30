1911ddf3358491fc517f0a5d302e880c918d26e5
Maurice Pakvis <0966972@hr.nl>
2020-11-30 16:01:45 +0100

    add UserDto

--- src/main/java/nl/hr/recipefinder/controller/UserController.java
+++ src/main/java/nl/hr/recipefinder/controller/UserController.java
@@ -1,5 +1,6 @@
 package nl.hr.recipefinder.controller;
 
+import nl.hr.recipefinder.model.dto.UserDto;
 import nl.hr.recipefinder.model.entity.User;
 import nl.hr.recipefinder.repository.UserRepository;
 import nl.hr.recipefinder.security.Role;
@@ -8,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.modelmapper.ModelMapper;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +23,8 @@ import java.util.Optional;
 public class UserController {
 
   private UserService userService;
+  @Autowired
+  ModelMapper modelMapper;
 
   @Autowired
   public UserController(UserService userService){
@@ -36,15 +41,21 @@ public class UserController {
   }
 
   @GetMapping("/users")
-  public List<User> getUsers() {
-      return userService.findAll();
+  public List<UserDto> getUsers() {
+    List<User> listUser = userService.findAll();
+    List<UserDto> listUserDTO = new ArrayList<UserDto>();
+    for(int i = 0; i < listUser.size(); i++){
+      listUserDTO.add(modelMapper.map(listUser.get(i), UserDto.class));
+    }
+    return listUserDTO;
   }
 
   @PostMapping("/users")
-  public boolean createUser(@RequestBody User user) {
+  public boolean createUser(@RequestBody UserDto userDto) {
+    User mappedUser = modelMapper.map(userDto, User.class);
     try {
-      if (user.getRole() == null) user.setRole(Role.USER);
-      userService.save(user);
+      if (mappedUser.getRole() == null) mappedUser.setRole(Role.USER);
+      userService.save(mappedUser);
       return true;
     }
     catch (Exception e){
---
+++ src/main/java/nl/hr/recipefinder/model/dto/UserDto.java
@@ -0,0 +1,41 @@
+package nl.hr.recipefinder.model.dto;
+
+import nl.hr.recipefinder.security.Role;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+public class UserDto {
+  private String username;
+
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+}
