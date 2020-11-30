e2bc7191d4d1bdb1e74382f4f95ecbb2072f0d12
Maurice Pakvis <0966972@hr.nl>
2020-11-30 16:49:29 +0100

    add lombock getter and setter

--- src/main/java/nl/hr/recipefinder/model/dto/UserDto.java
+++ src/main/java/nl/hr/recipefinder/model/dto/UserDto.java
@@ -1,10 +1,13 @@
 package nl.hr.recipefinder.model.dto;
 
 import nl.hr.recipefinder.security.Role;
-
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
+@Getter
+@Setter
 public class UserDto {
   private String username;
 
@@ -13,29 +16,4 @@ public class UserDto {
   @Enumerated(EnumType.STRING)
   private Role role;
 
-
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Role getRole() {
-    return role;
-  }
-
-  public void setRole(Role role) {
-    this.role = role;
-  }
 }
