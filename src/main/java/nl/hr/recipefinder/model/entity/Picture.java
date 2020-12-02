53b16327cc88e62e7692a2922c72c106078473cf
Maurice Pakvis <0966972@hr.nl>
2020-12-02 15:33:17 +0100

    De relatie tussen de objecten was niet goed, is nu verholpen

--- src/main/java/nl/hr/recipefinder/model/entity/Picture.java
+++ src/main/java/nl/hr/recipefinder/model/entity/Picture.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -13,6 +14,7 @@ import javax.persistence.ManyToOne;
 public class Picture extends BaseEntity {
   private String name;
   private Byte[] content;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "recipe_id")
   private Recipe recipe;
