47e1d3dafd7d32984f3a37384d76a3d83cf8a3a5
Maurice Pakvis <0966972@hr.nl>
2020-12-10 22:21:44 +0100

    push image as code

--- src/app/recipe-creator/recipe-creator.component.ts
+++ src/app/recipe-creator/recipe-creator.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {toBase64String} from "@angular/compiler/src/output/source_map";
 
 @Component({
   selector: 'app-recipe-creator',
@@ -24,10 +25,21 @@ export class RecipeCreatorComponent implements OnInit {
   }
 
   selectedFile: File = null;
+  test: string
 
   onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
   }
+  handleUpload(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      console.log(reader.result);
+      this.test = reader.result.toString();
+    };
+  }
+
 
   onUpload(){
     let url = 'http://localhost:8080/picture';
@@ -43,10 +55,10 @@ export class RecipeCreatorComponent implements OnInit {
 
   CreateRecipe() {
     let url = 'http://localhost:8080/recipe';
-    // let pictures = [{
-    //   name : this.model.pictures,
-    //   content : this.model.pictures.binaryType
-    // }]
+    let pictures = [{
+      name : 'test',
+      content : this.test.split(',')[1]
+    }]
     let token: string = '' + sessionStorage.getItem('token');
     const headers = new HttpHeaders({
       authorization: 'Basic ' + token
@@ -54,7 +66,8 @@ export class RecipeCreatorComponent implements OnInit {
     let postData = {
       name: this.model.name,
       description: this.model.description,
-      instructions: this.model.instructions
+      instructions: this.model.instructions,
+      pictures
     };
     this.http.post<Observable<boolean>>(url, postData, {headers: headers}).subscribe(isValid => {
       if (isValid) {
