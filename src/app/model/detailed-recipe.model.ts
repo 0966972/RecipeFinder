4d87db4e2377ba6a8a0e2c640b0e28c11eb67a1a
Simon Besseling <sbesseling@growteq.nl>
2021-01-03 21:27:56 +0100

    Added to details page

--- src/app/model/detailed-recipe.model.ts
+++ src/app/model/detailed-recipe.model.ts
@@ -9,7 +9,7 @@ export class DetailedRecipe implements Mappable {
   public instructions: string;
   public steps: Step[];
   public servings: number = 0;
-  public duration: number = 0;
+  public preparationTime: number = 0;
   public ingredients: Ingredient[];
   public pictures: string[][];
   public creator: string;
