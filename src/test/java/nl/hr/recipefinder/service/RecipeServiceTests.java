d0e1925ca4b3f66ffaf405fc8efcfdc8a920a09a
Simon Besseling <sbesseling@growteq.nl>
2021-01-10 19:09:02 +0100

    Added findByIdTest unit test

---
+++ src/test/java/nl/hr/recipefinder/service/RecipeServiceTests.java
@@ -0,0 +1,44 @@
+package nl.hr.recipefinder.service;
+
+import nl.hr.recipefinder.model.entity.Recipe;
+import nl.hr.recipefinder.repository.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceTests {
+
+  @Autowired
+  @InjectMocks
+  RecipeService recipeService;
+
+  @Mock
+  RecipeRepository recipeRepository;
+
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void findByIdTest(){
+    // arrange
+    long input = 1;
+    Optional<Recipe> recipe = Optional.of(new Recipe());
+    when(recipeRepository.findById(input)).thenReturn(recipe);
+
+    // act
+    Optional<Recipe> receivedRecipe = recipeService.findById(input);
+
+    // assert
+    assertEquals(recipe, receivedRecipe);
+  }
+}
