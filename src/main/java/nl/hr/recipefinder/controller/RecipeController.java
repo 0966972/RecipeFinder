14807c61d7376c09a30f854dc3927dfdc0cd1e9d
Simon Besseling <sbesseling@growteq.nl>
2021-01-10 17:15:54 +0100

    Removed unneeded import

--- src/main/java/nl/hr/recipefinder/controller/RecipeController.java
+++ src/main/java/nl/hr/recipefinder/controller/RecipeController.java
@@ -5,7 +5,6 @@ import nl.hr.recipefinder.model.dto.RecipeDto;
 import nl.hr.recipefinder.model.entity.Recipe;
 import nl.hr.recipefinder.model.entity.User;
 import nl.hr.recipefinder.model.httpexception.clienterror.HttpConflictError;
-import nl.hr.recipefinder.model.httpexception.clienterror.HttpNotFoundError;
 import nl.hr.recipefinder.model.httpexception.serverError.HttpInternalServerError;
 import nl.hr.recipefinder.service.RecipeService;
 import nl.hr.recipefinder.service.SessionService;
