1230e013e6ac47ab9db27cbea52e534f8a27a04f
MrTheGood <maarten@degoedeweb.nl>
2021-01-17 19:52:25 +0100

    Add minimum score filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,63 +6,88 @@ import {Ingredient} from "../model/ingredient.model";
 import {IngredientService} from "../service/ingredient.service";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
 
-  recipes: ListedRecipe[];
-  picture: ''
-  searchInput: '' = ''
-  filterIngredients: Ingredient[] = []
-  ingredientOptions: any[] = []
+    recipes: ListedRecipe[];
+    picture: ''
+    searchInput: '' = ''
+    doFilterScore = false
+    scoreFilterNum = 1
+    scoreFilter = []
+    filterIngredients: Ingredient[] = []
+    ingredientOptions: any[] = []
 
-  constructor(
-    private recipeService: RecipeService,
-    private ingredientService: IngredientService,
-    private router: Router,
-  ) {
-  }
+    constructor(
+        private recipeService: RecipeService,
+        private ingredientService: IngredientService,
+        private router: Router,
+    ) {
+    }
 
-  ngOnInit() {
-    this.recipeService.findAll().subscribe(data => {
-      this.recipes = data
-    });
-  }
+    ngOnInit() {
+        this.recipeService.findAll().subscribe(data => {
+            this.recipes = data
+        });
+    }
 
-  openRecipe(id: number) {
-    this.router.navigate(['recipe/' + id])
-  }
-
-
-  findIngredient(i) {
-    this.ingredientOptions = [];
-
-    this.ingredientService.search(this.filterIngredients[i].name).subscribe(options => {
-      this.ingredientOptions = options
-    });
-
-    this.searchInputChanged()
-  }
+    openRecipe(id: number) {
+        this.router.navigate(['recipe/' + id])
+    }
 
 
-  addIngredient() {
-    this.filterIngredients.push({
-      id: null,
-      name: null,
-    })
-  }
+    findIngredient(i) {
+        this.ingredientOptions = [];
 
-  removeIngredient(i: number) {
-    this.filterIngredients.splice(i, 1);
-    this.searchInputChanged()
-  }
+        this.ingredientService.search(this.filterIngredients[i].name).subscribe(options => {
+            this.ingredientOptions = options
+        });
 
-  searchInputChanged() {
-    this.recipeService.search(this.searchInput, this.filterIngredients).subscribe(data => {
-      this.recipes = data;
-    });
-  }
+        this.searchInputChanged()
+    }
+
+
+    addIngredient() {
+        this.filterIngredients.push({
+            id: null,
+            name: null,
+        })
+    }
+
+    removeIngredient(i: number) {
+        this.filterIngredients.splice(i, 1);
+        this.searchInputChanged()
+    }
+
+
+    showScoreFilter(show: boolean) {
+        this.doFilterScore = show
+        if (show) {
+            this.scoreFilter = [true, false, false, false, false]
+            this.scoreFilterNum = 1
+        } else {
+            this.scoreFilter = []
+            this.scoreFilterNum = null
+        }
+        this.searchInputChanged()
+    }
+
+    scoreSelected(i) {
+        this.scoreFilterNum = i;
+        this.scoreFilter = []
+        for (let j = 0; j < 5; j++)
+            this.scoreFilter.push(j <= i);
+        this.searchInputChanged()
+    }
+
+
+    searchInputChanged() {
+        this.recipeService.search(this.searchInput, this.filterIngredients, this.scoreFilterNum).subscribe(data => {
+            this.recipes = data;
+        });
+    }
 }
